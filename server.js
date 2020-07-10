b22eb1752870c9394f9e8bf9bdbf6897e6c71f91
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2020-07-10 15:05:58 -0700

    Server setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express'); 
+const app = express(); 
+const methodOverride = require('method-override'); 
+const PORT = process.env.PORT || 4000; 
+
+// --- View Engine --- // 
+app.set('view engine', 'ejs')
+
+// --- Middleware --- // 
+
+// Method Override
+app.use(methodOverride('__method')); 
+
+// Serve Static Assets (Front End JavaScript, CSS, Images, etc)
+app.use(express.static(`${__dirname}/public`));
+
+// Express BodyParser
+app.use(express.urlencoded({extended:false})); 
+
+// Custom Logger
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url} ${new Date().toLocaleTimeString()}`); 
+    next(); 
+}); 
+
+// --- Routes --- // 
+
+// --- Server Listener --- // 
+app.listen(PORT, () => {console.log(`Server is listening on ${PORT}...`)})
